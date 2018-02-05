7b24e52e9197c1a989cec44ac603759745e03525
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-02-05 16:29:46 +0530

    array
    code

---
+++ arrl.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int A[25],size,max,c;
+printf("\n enterthe size of thearray:");
+scanf("%d",&size);
+printf("\n enter the integers for the array");
+for(c=o;c<size;c++)
+scanf("%d",&arr[c]);
+max=array[0];
+for(c=1;c<size;c++)
+{
+if(array[c]>max)
+{
+max=array[c];
+}
+}
+printf("\n maxelement present in the array is %d",max);
+getch();
+}
