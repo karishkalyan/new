737e64d2d9152c6e5ed4f5fa3beda3ed24dacc70
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-02-05 16:32:47 +0530

    minimum
    code

---
+++ arraymin.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int A[25],size,min,c;
+printf("\n enterthe size of thearray:");
+scanf("%d",&size);
+printf("\n enter the integers for the array");
+for(c=o;c<size;c++)
+scanf("%d",&arr[c]);
+min=array[0];
+for(c=1;c<size;c++)
+{
+if(array[c]<min)
+{
+min=array[c];
+}
+}
+printf("\n minimum element present in the array is %d",max);
+return 0;
+}
